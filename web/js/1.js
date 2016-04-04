839c3d454646aeaf4d12d55eee6f8f6debff895d
Mary Orlova <mary.orlova30@gmail.com>
2016-04-04 13:38:22 +0400

    update

--- web/js/1.js
+++ web/js/1.js
@@ -42,10 +42,9 @@ $(document).ready(function() {
             return /\d{1,2}\.\d{1,2}\.\d{1,4} \d{1,2}:\d{1,2}/.test(s);
         },
         format: function(s) {
-            s = s.replace(/\-/g," ");
+
             s = s.replace(/:/g," ");
             s = s.replace(/\./g," ");
-            s = s.replace(/\//g," ");
             s = s.split(" ");
             return $.tablesorter.formatFloat(new Date(s[2], s[1]-1, s[0], s[3], s[4]).getTime());
         },
